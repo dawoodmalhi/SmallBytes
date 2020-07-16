37a6bd475322f6913974521bfc91c3487460dc7c
Tayyab-Asghar <muhammadtayyabasghar@gmail.com>
2020-07-17 02:36:32 +0500

    Modifing Dawood Code

--- Lib/Compressor.js
+++ Lib/Compressor.js
@@ -1,14 +1,15 @@
-compressorLZW = (input) =>{ 
+module.exports.compressorLZW = input => { 
     // initializing Dictionary of ASCII extended characters
     let dicCapacity = 256;
     let dictionary = {};
-    for(let i=0;i<dicCapacity;++i)
+    
+    for (let i=0; i < dicCapacity; ++i)
         dictionary[String.fromCharCode(i)] = i;
     
     let series = input[0];
     let output = [];
 
-    for(let i=1;i< input.length;++i){
+    for(let i=1; i< input.length; ++i){
         if(series+String.fromCharCode(input[i]) in dictionary)
             series += String.fromCharCode(input[i]);
         else{
@@ -22,4 +23,37 @@ compressorLZW = (input) =>{
     return output;
 }
 
-module.exports = compressorLZW;
+// module.exports.encode = Data =>
+// {
+//     try
+//     {
+//         let dictionary = {},
+//             out = [],
+//             currentChar,
+//             phrase = Data[0],
+//             code = 57344;
+//         Data = (Data + "").split("");
+    
+//         for (let i = 1; i < Data.length; i++)
+//         {
+//             currentChar = Data[i];
+            
+//             if (dictionary[phrase + currentChar] != null)
+//                 phrase += currentChar;
+//             else
+//             {
+//                 out.push(phrase.length > 1 ? dictionary[phrase] : phrase.codePointAt(0));
+//                 dictionary[phrase + currentChar] = code;
+//                 code++;
+//                 phrase = currentChar;
+//             }
+//         }
+//         out.push(phrase.length > 1 ? dictionary[phrase] : phrase.codePointAt(0));
+        
+//         return out.map(e => String.fromCodePoint(e)).join('');
+//     }
+//     catch (error)
+//     {
+//         throw new Error(error);
+//     }
+// }
