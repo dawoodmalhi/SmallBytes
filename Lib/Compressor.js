66e556cee159b263e88606da51bc58ed4ca55ed1
dawoodmalhi <dawoodmalhi007@gmail.com>
2020-07-17 01:52:58 +0500

    implemented the function

---
+++ Lib/Compressor.js
@@ -0,0 +1,26 @@
+
+compressorLZW = (input) =>{ 
+    // initializing Dictionary of ASCII extended characters
+    let dicCapacity = 256;
+    let dictionary = {};
+    for(let i=0;i<dicCapacity;++i)
+        dictionary[String.fromCharCode(i)] = i;
+    
+    let series = input[0];
+    let output = [];
+
+    for(let i=1;i< input.length;++i){
+        if(series+String.fromCharCode(input[i]) in dictionary)
+            series += String.fromCharCode(input[i]);
+        else{
+            dictionary[series+String.fromCharCode(input[i])] = dicCapacity++;
+            output.push(dictionary[series]);
+            series = String.fromCharCode(input[i]);
+        }
+    }
+    output.push(dictionary[series]);
+    output.join('');
+    return output;
+}
+
+module.exports = compressorLZW;
