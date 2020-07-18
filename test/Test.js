54585b6ffae5dbd2b83015ae22f925fb83afbca7
dawoodmalhi <dawoodmalhi007@gmail.com>
2020-07-18 22:04:56 +0500

    Classifying testing

--- test/Test.js
+++ test/Test.js
@@ -1,18 +1,23 @@
 const fs = require('fs');
 const compress = require("../lib/Compressor")
-const decompress = require("../lib//Decompressor")
+const decompress = require("../lib/Decompressor")
 
-const inputFile = "test.txt";
-const outputFile = "tested.txt";
+const inputFile = "testSmall.txt";
+const outputFile = "testResult.txt";
 
 let data = fs.readFileSync(inputFile, 'utf8');
 
+//let data = "abacabcabc";
+// Expected output = [97,98,97,99,256,259,98,99]
+
 let EncodedData = compress.compressorLZW(data)
 
-fs.writeFileSync(outputFile, EncodedData);
+let outputToBeWritten = EncodedData.join('\r\n');
 
-// let DecodedData = decompress.decode(EncodedData)
+fs.writeFileSync(outputFile, outputToBeWritten);
 
-// //fs.writeFileSync(outputFile, DecodedData);
+// let DecodedData = decompress.decompressorLZW(EncodedData)
+
+// fs.writeFileSync(outputFile, DecodedData);
 
 // console.log(DecodedData);
