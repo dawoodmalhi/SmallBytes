f781f52f66c58b7c3a5ecd1c46ccf149ff255b33
Tayyab-Asghar <muhammadtayyabasghar@gmail.com>
2020-07-25 21:57:08 +0500

    Completed compressor but still some problems

--- Lib/Compressor.js
+++ Lib/Compressor.js
@@ -4,28 +4,35 @@ module.exports.compressorLZW = input =>
     {
         if (!input) return input;       // Checking the empty input
 
-        var dict = []
-        var data = (input + "").split("");             // Slicing data into characters 
-        var compressed = [];                           // Output Array
-        var currChar;
-        var phrase = data[0];                         // Works as an element of dictionary
-        var code = 256;                               // Starting Dictionary
+        let dict = new Map();
+        let data = (input + "").split("");             // Slicing data into characters 
+        let compressed = [];                           // Output Array
+        let currChar;
+        let phrase = data[0];                         // Works as an element of dictionary
+        let code = 256;                               // Starting Dictionary
 
 
-        for(let i=1; i< input.length; i++)
+        for(let i=1; i < input.length; i++)
         {
             if (dict.has(phrase + currChar))            // If dictionary has that combination of characters just add it into phrase
                 phrase += currChar;
             else
             {
-                dictionary[phrase+input[i]] = code++;
-                output.push(dictionary[phrase]);
-                phrase = input[i];
+                compressed.push (phrase.length > 1 ? dict.get(phrase) : phrase.codePointAt(0));
+                dict.set(phrase + currChar, code);
+                code++;
+
+                if (code === 0xd800)
+                    code = 0xe000;
+                phrase = currChar;
             }
         }
-        output.push(dictionary[phrase]);
-        output.join('');
-        return output;
+        compressed.push (phrase.length > 1 ? dict.get(phrase) : phrase.codePointAt(0));
+        
+        for (let i = 0; i < compressed.length; i++)
+            compressed[i] = String.fromCodePoint(compressed[i]);
+
+        return compressed.join("");
     }
     catch (error)
     {
--- test/Test.js
+++ test/Test.js
@@ -1,23 +1,23 @@
-const fs = require('fs');
-const compress = require("../lib/Compressor")
-const decompress = require("../lib/Decompressor")
+// const fs = require('fs');
+// const compress = require("../lib/Compressor")
+// const decompress = require("../lib/Decompressor")
 
-const inputFile = "testSmall.txt";
-const outputFile = "testResult.txt";
+// const inputFile = "testSmall.txt";
+// const outputFile = "testResult.txt";
 
-// let data = fs.readFileSync(inputFile, 'utf8');
+// // let data = fs.readFileSync(inputFile, 'utf8');
 
-//let data = "abacabcabc";
-// Expected output = [97,98,97,99,256,259,98,99]
+// //let data = "abacabcabc";
+// // Expected output = [97,98,97,99,256,259,98,99]
 
-let EncodedData = compress.compressorLZW(data)
+// let EncodedData = compress.compressorLZW(data)
 
-let outputToBeWritten = EncodedData.join('\r\n');
+// let outputToBeWritten = EncodedData.join('\r\n');
 
-fs.writeFileSync(outputFile, outputToBeWritten);
+// fs.writeFileSync(outputFile, outputToBeWritten);
 
-// let DecodedData = decompress.decompressorLZW(EncodedData)
+// // let DecodedData = decompress.decompressorLZW(EncodedData)
 
-// fs.writeFileSync(outputFile, DecodedData);
+// // fs.writeFileSync(outputFile, DecodedData);
 
-// console.log(DecodedData);
+// // console.log(DecodedData);
