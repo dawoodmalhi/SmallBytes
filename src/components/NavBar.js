dec5dfd29fb4b5f039bf4b96a5dcecc4c72ecbef
dawoodmalhi <dawoodmalhi007@gmail.com>
2020-07-26 14:10:49 +0500

    Navigation Bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,8 +6,25 @@ class NavBar extends React.Component {
     render() {
       return (
         <div>
-          
-          </div>
+          <header class="text-gray-700 body-font">
+            <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+              <a href="#" class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+                <img src={logo} alt="Logo" height="60" width="60" ></img>
+                <span class="ml-5 text-xl">Small Bytes</span>
+              </a>
+              <nav class="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
+                <a href="#" class="mr-5 ">Home</a>
+                <a href="#" class="mr-5 ">Compressor</a>
+                <a href="#" class="mr-5 ">Decompressor</a>
+              </nav>
+              <button class="inline-flex items-center bg-gray-200 border-0 py-1 px-3 focus:outline-none rounded text-base mt-4 md:mt-0">Button
+                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-1" viewBox="0 0 24 24">
+                  <path d="M5 12h14M12 5l7 7-7 7"></path>
+                </svg>
+              </button>
+            </div>
+          </header>
+          <hr></hr>
         </div>
       );
     }
