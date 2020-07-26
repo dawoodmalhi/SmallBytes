918292af8f5e048cc30546019afa972dbc4655ce
dawoodmalhi <dawoodmalhi007@gmail.com>
2020-07-26 12:13:56 +0500

    started the development in react

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,19 +6,7 @@ class NavBar extends React.Component {
     render() {
       return (
         <div>
-          <div className="navbar">
-            <img src={logo} alt="Small Bytes Logo"></img>
-            <ul>
-              <li>
-                <a href="#">Home</a>
-              </li>
-              <li>
-                <a href="#">Compressor</a>
-              </li>
-              <li>
-                <a href="#">Decompressor</a>
-              </li>
-            </ul>
+          
           </div>
         </div>
       );
