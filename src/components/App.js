10e3d99603fa5976f80e1958ed7cc08a293ee45e
dawoodmalhi <dawoodmalhi007@gmail.com>
2020-07-28 19:06:00 +0500

    Footer

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "../styles/App.css";
 import NavBar from "./NavBar";
 import IntroBanner from "./IntroBanner"
+import Footer from "./Footer"
 
 const App = () => {
     return (
       <div className="body">
         <NavBar />
         <IntroBanner />
+        <Footer />
       </div>
     );
   };
