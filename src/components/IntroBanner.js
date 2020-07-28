820255ef8d31cac3c3a114f8ebfd9c0131cff8db
dawoodmalhi <dawoodmalhi007@gmail.com>
2020-07-28 19:04:12 +0500

    Introduction Page

--- src/components/IntroBanner.js
+++ src/components/IntroBanner.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "../styles/IntroBanner.css";
-import logo  from "../images/SmallBytesLogo.SVG"
+import logo  from "../images/SmallBytesLogo.SVG";
 
 class IntroBanner extends React.Component {
     render(){
         return(
             <div>
                 <section className="text-gray-700 body-font">
-                    <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+                    <div className="container mx-auto flex px-5 py-20 md:flex-row flex-col items-center">
                         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
                             <h1 className="title-font sm:text-4xl text-3xl mb-4 font-semibold text-gray-900" style={{color: "#db2d2c"}}>
                                 What is Small Bytes
@@ -39,6 +39,7 @@ class IntroBanner extends React.Component {
                         </div>
                     </div>
                 </section>
+                <hr></hr>
             </div>
         );
     }
