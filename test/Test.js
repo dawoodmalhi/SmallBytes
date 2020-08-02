ad890d220ec00613ee31f431a001d4c458b936b0
Tayyab-Asghar <muhammadtayyabasghar@gmail.com>
2020-08-02 23:07:35 +0500

    Tried to implement AC still alot of unknown bugs

--- test/Test.js
+++ test/Test.js
@@ -1,1 +1,7 @@
 const fs = require('fs');
+const encode = require('../lib/Arithmetic-Coding/encode')
+const decode = require('../lib/Arithmetic-Coding/decode')
+
+encode.encode(path.resolve(__dirname, './txt/testResult.txt'), path.resolve(__dirname, './txt/short-encoded.ac'))
+
+decode.decode(path.resolve(__dirname, './txt/short-encoded.ac'), path.resolve(__dirname, './txt/tested.txt'))
