3d54ed56634cd8bfeff2435502d2a87961767c8b
Tayyab-Asghar <muhammadtayyabasghar@gmail.com>
2020-08-07 20:12:20 +0500

    Still size is not good

--- test/Test.js
+++ test/Test.js
@@ -1,7 +1,7 @@
-const fs = require('fs');
 const encode = require('../lib/Arithmetic-Coding/encode')
 const decode = require('../lib/Arithmetic-Coding/decode')
+const path = require('path')
 
-encode.encode(path.resolve(__dirname, './txt/testResult.txt'), path.resolve(__dirname, './txt/short-encoded.ac'))
+encode.encode(path.resolve(__dirname, './testLarge.txt'), path.resolve(__dirname, './short-encoded.ac'))
 
-decode.decode(path.resolve(__dirname, './txt/short-encoded.ac'), path.resolve(__dirname, './txt/tested.txt'))
+decode.decode(path.resolve(__dirname, './short-encoded.ac'), path.resolve(__dirname, './tested.txt'))
